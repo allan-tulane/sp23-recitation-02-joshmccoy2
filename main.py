d5f4bfe228b58ac8c773e4b7088d853df8e570d7
Josh McCoy <112521093+joshmccoy2@users.noreply.github.com>
2023-02-11 01:43:03 +0000

    V2

--- main.py
+++ main.py
@@ -2,102 +2,81 @@
 CMPS 2200  Recitation 2
 """
 
-### the only imports needed are here
 import tabulate
 import time
-###
 
 def simple_work_calc(n, a, b):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + n
-
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
+  if n == 1:
+    return 1
+  elif n ==0:
+    return 0
+  else:
+    return a*simple_work_calc(n/b, a, b)+ n
+pass
 
 def test_simple_work():
-	""" done. """
-	assert work_calc(10, 2, 2) == #TODO
-	assert work_calc(20, 3, 2) == #TODO
-	assert work_calc(30, 4, 2) == #TODO
-
+	assert work_calc(10, 2, 2) == 36
+	assert work_calc(20, 3, 2) == 230
+	assert work_calc(30, 4, 2) == 650
+  #assert work_calc(30, 4, 2) == 
+  #assert work_calc(30, 4, 2) == 
+  #assert work_calc(30, 4, 2) == 
+# Dealing with indentation error
 def work_calc(n, a, b, f):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
-
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-	f......a function that takes an integer and returns 
-           the work done at each node 
-
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
+  if n == 1:
+    return 1
+  elif n == 0:
+    return 0
+  else: return a*work_calc(n//b, a, b, f)+ f(n)
+pass
 
 def span_calc(n, a, b, f):
-	"""Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
-
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-	f......a function that takes an integer and returns 
-           the work done at each node 
-
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
+  if n == 1:
+    return 1
+  elif n == 0:
+    return 0
+  else: return work_calc(n//b, a, b, f) + f(n)
+pass
 
 def test_work():
-	""" done. """
-	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
-	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
-	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+	assert work_calc(10, 2, 2,lambda n: 1) == 15
+	assert work_calc(20, 1, 2, lambda n: n*n) == 530
+	assert work_calc(30, 3, 2, lambda n: n) == 300
+ # assert work_calc(40, 2, 3, lambda 
+ # assert work_calc(50, 3, 4, lambda 
+ # assert work_calc(40, 2, 3, lambda 
+  # still having indentation error like last lab
+
 
 def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
-	"""
-	Compare the values of different recurrences for 
-	given input sizes.
-
-	Returns:
-	A list of tuples of the form
-	(n, work_fn1(n), work_fn2(n), ...)
-	
-	"""
 	result = []
-	for n in input_sizes:
-		# compute W(n) using current a, b, f
-		result.append((
-			n,
-			work_fn1(n),
-			work_fn2(n)
-			))
+	for n in sizes:
+		result.append((n, work_fn1(n), work_fn2(n)))
 	return result
-
+	
 def print_results(results):
-	""" done """
 	print(tabulate.tabulate(results,
 							headers=['n', 'W_1', 'W_2'],
 							floatfmt=".3f",
 							tablefmt="github"))
 
-def test_compare_work():
-	# curry work_calc to create multiple work
-	# functions taht can be passed to compare_work
-    
-	# create work_fn1
-	# create work_fn2
+def test_compare_work(sizes =[10,20,50,100,1000,5000, 10000]):
+  result = []
+  work_fn1 = lambda n: work_calc(n, 4, 2, lambda n: n)
+  work_fn2 = lambda n: work_calc(n, 4, 2, lambda n:n*n)
+  res = compare_work(work_fn1, work_fn2)
+  print_results(res)
 
-    res = compare_work(work_fn1, work_fn2)
-	print(res)
+def compare_span(span_fn1, span_fn2, sizes=[10,20,50,100,1000,5000,10000]):
+  result = []
+  for n in sizes:
+    result.append((n, work_fn1(n), work_fn2(n)))
+  return result
+  
 
 def test_compare_span():
-	# TODO
+  result = []
+  span_fn1 = lambda n: span_calc(n, 4, 2, lambda n: n)
+  span_fn2 = lambda n: span_calc(n, 4, 2, lambda n: n*n)
+  res = compare_work(span_fn1, span_fn2)
+  print_results(res)
